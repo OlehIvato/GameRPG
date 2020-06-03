28e6bebb41c4aae0cfc57a8de90f4a4e4546e471
doc <docwff@gmail.com>
2020-06-03 15:36:04 +0300

    test

--- src/Main/java/spring/repository/Equipment_ArmorsRepository.java
+++
@@ -1,7 +0,0 @@
-package spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import spring.model.Equipment_Armors;
-
-public interface Equipment_ArmorsRepository extends JpaRepository<Equipment_Armors, Long> {
-}
--- src/Main/java/spring/repository/Equipment_TypesRepository.java
+++
@@ -1,7 +0,0 @@
-package spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import spring.model.Equipment_Types;
-
-public interface Equipment_TypesRepository extends JpaRepository<Equipment_Types, Long> {
-}
